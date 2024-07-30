9f99602de597c52fb7164fd99d06f7f84d021a3b
Muhammad Talha Saeed <83191199+Talha-Ahrar@users.noreply.github.com>
2024-07-30 15:23:24 +0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { TimetableComponent } from './pages/timetable/timetable.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'timetable', component: TimetableComponent },
+
+
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
